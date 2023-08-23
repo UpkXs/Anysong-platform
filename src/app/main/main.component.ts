83f899e82d5618881cb991256dff88acf5e7ce95
asugurali <asugurali@greet-go.com>
2023-08-23 17:36:51 +0600

    feat(): now main component includes two buttons to choose 'by Artist or by Song'

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  byArtist: string = 'by Artist';
+  bySong: string = 'by Song';
 
   constructor() { }
 
