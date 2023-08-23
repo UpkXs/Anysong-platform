3ca8cbad06cec68aea56f28de8c1a52f677202f9
asugurali <asugurali@greet-go.com>
2023-08-23 17:34:22 +0600

    feat(): generated button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { ButtonComponent } from './widgets/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
