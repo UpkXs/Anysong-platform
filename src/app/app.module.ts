3528525eb2b67e5653348e9d53132e5472f769f5
asugurali <asugurali@greet-go.com>
2023-08-25 14:17:10 +0600

    fix(): removed circle button with numeric component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ButtonComponent } from './widgets/button/button.component';
 import { PlaygroundComponent } from './playground/playground.component';
-import { CircleButtonWithNumericComponent } from './widgets/circle-button-with-numeric/circle-button-with-numeric.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ButtonComponent,
-    PlaygroundComponent,
-    CircleButtonWithNumericComponent
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule,
