938d46de49d1d476d94b9a9e5e64c4e014bcabd0
asugurali <asugurali@greet-go.com>
2023-08-25 11:03:50 +0600

    feat(): generated playground component with some example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ButtonComponent } from './widgets/button/button.component';
+import { PlaygroundComponent } from './playground/playground.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ButtonComponent
+    ButtonComponent,
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule,
