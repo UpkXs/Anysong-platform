fa59ab3fcc377acf72a16eb0c66af3454b191694
asugurali <asugurali@greet-go.com>
2023-08-25 11:07:22 +0600

    feat(): generated circle button with numeric component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ButtonComponent } from './widgets/button/button.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { CircleButtonWithNumericComponent } from './widgets/circle-button-with-numeric/circle-button-with-numeric.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ButtonComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    CircleButtonWithNumericComponent
   ],
   imports: [
     BrowserModule,
