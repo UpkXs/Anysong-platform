5df75ae2851ceabea62de6efbeaad0567333afa8
asugurali <asugurali@greet-go.com>
2023-08-25 14:19:36 +0600

    feat(): generated octothorpe with numeric component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ButtonComponent } from './widgets/button/button.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { OctothorpeWithNumericComponent } from './widgets/octothorpe-with-numeric/octothorpe-with-numeric.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ButtonComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    OctothorpeWithNumericComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/widgets/octothorpe-with-numeric/octothorpe-with-numeric.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'anysong-octothorpe-with-numeric',
+  templateUrl: './octothorpe-with-numeric.component.html',
+  styleUrls: ['./octothorpe-with-numeric.component.scss']
+})
+export class OctothorpeWithNumericComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
